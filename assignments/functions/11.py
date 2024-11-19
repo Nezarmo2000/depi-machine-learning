9f05e2c277a824c8e2d449b2385fc470caf308f6
Nezarmo2000 <73034116+Nezarmo2000@users.noreply.github.com>
2024-11-20 00:38:00 +0200

    Update 11.py

--- assignments/functions/11.py
+++ assignments/functions/11.py
@@ -6,4 +6,4 @@ for num in numbers:
     if num > max_value:
         max_value = num
 
-print("The maximum value in the list is:", max_value)
+print(f"The maximum value in the list is: {max_value}")
