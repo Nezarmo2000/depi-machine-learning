5174be5be91c93aabc331ea7506ee972c514001b
Nezarmo2000 <73034116+Nezarmo2000@users.noreply.github.com>
2024-11-19 22:29:14 +0200

    Delete 5.py

--- 5.py
+++
@@ -1,8 +0,0 @@
-# Accept a word from the user
-word = input("Enter a word: ")
-
-# Reverse the word using slicing
-reversed_word = word[::-1]
-
-# Display the reversed word
-print("The reversed word is:", reversed_word)
