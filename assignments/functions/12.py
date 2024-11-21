097dec615cdd6574d2a3e410e9318e36a3443e9a
Nezarmo2000 <73034116+Nezarmo2000@users.noreply.github.com>
2024-11-21 17:08:14 +0200

    Update 12.py

--- assignments/functions/12.py
+++ assignments/functions/12.py
@@ -7,3 +7,10 @@ for char in text:
         count += 1
 
 print(f"The number of vowels in the string is: {count}")
+
+"""
+text = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+num =list(filter(lambda char: char in vowels, text))
+print(f"The number of vowels in the string is: {len(num)}")
+"""
