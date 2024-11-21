8c0d9eb9b06376ebb84b8ece87c6608caa66354a
Nezarmo2000 <73034116+Nezarmo2000@users.noreply.github.com>
2024-11-21 15:30:33 +0200

    Update 6.py

--- assignments/functions/6.py
+++ assignments/functions/6.py
@@ -1,11 +1,7 @@
 x=int(input("Enter the first number in series is "))
 y=int(input("Enter the last number in series is "))
-num_even=0
-num_odd=0
-for i in range(x,y+1):
-    if i%2==0:
-        num_even+=1
-    else :
-        num_odd+=1
-print(f"Number  of even numbers is {num_even}" )
-print(f"Number of odd numbers is {num_odd}" )
+numbers=list(range(x,y+1))
+even=list(filter(lambda x:x%2==0,numbers))
+odd=list(filter(lambda x:x%2!=0,numbers))
+print(f"Number  of even numbers is {len(even)}" )
+print(f"Number of odd numbers is {len(odd)}" )
